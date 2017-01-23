fc2bd4d17c66b4ca0df18e144729af1403a77d9f
MarxJiao <marxjiao@gmail.com>
2017-01-23 16:46:03 +0800

    use plugin

--- test/unit/viscroll.spec.js
+++ test/unit/viscroll.spec.js
@@ -10,6 +10,18 @@ import viscroll from '../../src/index.js';
 
 describe('viscroll directive test', () => {
     it('visroll shoud work fun', () => {
+        let com = {
+            template: `<div v-iscroll><div>{{text}}</div></div>`,
+            data() {
+                return {
+                    text: 'hello word'
+                }
+            }
+        }
+
+        Vue.use(viscroll)
+        let a = new Vue(com).$mount();
+        // console.log(a.$options.components);
         expect(true).toEqual(true);
     })
 });
